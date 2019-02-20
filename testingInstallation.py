e90627ade97bd70ba547da539f8fb4259a9f51ed
madurasenadeera <madura.sena@outlook.com>
2019-02-20 19:21:01 +0800

    confirmed tensorflow installation

--- testingInstallation.py
+++ testingInstallation.py
@@ -1,9 +1,12 @@
 import tensorflow as tf
 
+
+# have to always run the the classic phrase
 hello = tf.constant('hello world')
 
-
+# initialising a TensorFlow session to run text
+# (if session not there prints out tensor values)
 sess = tf.Session()
 
-
+# printing
 print(sess.run(hello))
